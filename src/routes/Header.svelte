20590244797dcc31dd66e16a6fb2adcabd8c4a74
Lim Bodhi Wijaya <lim.bodhi@ui.ac.id>
2024-01-29 11:57:03 +0700

    update 29 jan 2024

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,102 +2,15 @@
 	import logo from '$lib/images/TASLA logo.png';
 </script>
 
-<header>
-	<div class="corner">
-		<img src={logo} alt="TASLA"/>
-	</div>
+<header class="flex justify-between items-center bg-black py-2 px-4">
+	<img src="{logo}" alt="Logo" class="w-[138px] h-[34px]" />
 
-	<nav class="corner">
-		<ul>
-			<li>
-				<a href="/" class="title">Home</a>
-			</li>
-			<li>
-				<a href="/" class="title">Our Organization</a>
-			</li>
-			<li>
-				<a href="/" class="title">Scholars</a>
-			</li>
-			<li>
-				<a href="/" class="title">Sponsors</a>
-			</li>
+	<nav>
+		<ul class="flex list-none gap-4">
+			<li><a href="/" class="text-white text-lg font-normal no-underline font-primary hover:opacity-80">Home</a></li>
+			<li><a href="/our-organization" class="text-white text-lg font-normal no-underline font-primary hover:opacity-80">Our Organization</a></li>
+			<li><a href="/scholars" class="text-white text-lg font-normal no-underline font-primary hover:opacity-80">Scholars</a></li>
+			<li><a href="/sponsors" class="text-white text-lg font-normal no-underline font-primary hover:opacity-80">Sponsors</a></li>
 		</ul>
 	</nav>
 </header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	.title {
-		color: #F5F5F5;
-		font-weight: 600;
-		font-size: 1em;
-	}
-
-	.corner {
-		align-items: center;
-		width: auto;
-		height: 4em;
-		color: black;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		padding-left: 2em;
-		width: 8em;
-		height: 6em;
-		object-fit: contain;
-	}
-
-	/* svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	} */
-
-	ul {
-		height: 5em;
-		padding-right: 2em;
-		display: flex;
-		align-items: center;
-		list-style: none;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
--- src/routes/Main.svelte
+++ src/routes/Main.svelte
@@ -1,40 +1,96 @@
 <script>
     import foto from '$lib/images/Offline Leadership UI.svg';
+    import asset1 from '$lib/images/homepage_asset1.png';
+    import asset2 from '$lib/images/homepage_asset2.png';
+    import asset3 from '$lib/images/homepage_asset3.png';
+    import asset4 from '$lib/images/homepage_asset4.png';
+    import asset5 from '$lib/images/bxs-bank 1.svg';
+    import asset6 from '$lib/images/bx-book-open 1.svg';
+    import asset7 from '$lib/images/bx-chat 1.svg';
 </script>
-<h1>
-    Where Future Leaders Unlock Their Potential
-</h1>
-<p>
-    TASLA is a dedicated scholarship to help you unlock your leadership potentials.
-</p>
+<div class="section one bg-black py-5">
+  <div class="text-center text-neutral-100 text-4xl font-black font-primary">Where Future Leaders Unlock Their Potentials</div>
+  <div class="text-center text-neutral-100 text-2xl font-primary">TASLA is a dedicated scholarship to help you unlock your leadership potentials.</div>
+  <div class="flex items-center justify-center rounded-md">
+    <img src={foto} alt="TASLA Offline Leadership Program at UI" class="py-4 bg-opacity-50">
+  </div>
+</div>
 
-<img src={foto} alt="TASLA Offline Leadership Program at UI"/>
+<div class="h-[75px] transition bg-gradient-to-b from-black to-gray-700 shadow"></div>
 
-<style>
-    h1 {
-        color: #F5F5F5;
-        text-align: center;
-        /* font-size: 2em; */
-        font-weight: 900;
-        line-height: normal;
-        letter-spacing: -1.4px;
-        padding-top: 2em;
-    }
-    p {
-        color: #F5F5F5;
-        text-align: center;
-        font-size: 1.2em;
-        font-weight: 400;
-        letter-spacing: -0.4px;
-    }
+<div class="section two bg-gray-700 shadow px-10">
+  <div class="py-1 text-left text-white text-4xl font-black font-primary">Experience leadership by practically doing it</div>
+  <div class="py-1 text-left text-white text-xl font-primary">TASLA helps unlocking your leadership potentials through viral leadership, you will be exposed to maintain 4 of the following programs.</div>
+  <div class="grid grid-cols-4 py-10">
+    <div class="w-3/4 mx-auto">
+      <div class="relative aspect-w-3 aspect-h-4">
+        <img src="{asset1}" alt="Mentorship Program" class="object-cover w-full h-full">
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <span class="text-white text-2xl text-center font-bold font-primary">Mentorship Program</span>
+        </div>
+      </div>
+    </div>
+    <div class="w-3/4 mx-auto">
+      <div class="relative aspect-w-3 aspect-h-4">
+        <img src="{asset2}" alt="primarynship Placement" class="object-cover w-full h-full">
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <span class="text-white text-2xl text-center font-bold font-primary">Internship Placement</span>
+        </div>
+      </div>
+    </div>
+    <div class="w-3/4 mx-auto">
+      <div class="relative aspect-w-3 aspect-h-4">
+        <img src="{asset3}" alt="Leadership Training" class="object-cover w-full h-full">
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <span class="text-white text-2xl text-center font-bold font-primary">Leadership Training</span>
+        </div>
+      </div>
+    </div>
+    <div class="w-3/4 mx-auto">
+      <div class="relative aspect-w-3 aspect-h-4">
+        <img src="{asset4}" alt="Community Service" class="object-cover w-full h-full">
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+          <span class="text-white text-2xl text-center font-bold font-primary">Sharing Session Program</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-    img {
-		width: 700px;
-        height: 400px;
-        flex-shrink: 0;
-        object-fit: contain;
-        margin-left: 37em;
-        border-radius: 10px;
-        background: radial-gradient(50% 50% at 50% 50%, rgba(24, 24, 26, 0.00) 0%, rgba(24, 24, 26, 0.50) 100%);
-	}
-</style>
+<div class="section three bg-gray-700 shadow py-5 px-10">
+  <div class=" text-white text-4xl font-black font-primary">What are the benefits of being a TASLA scholar?</div>
+  <div class=" break-all text-white text-xl font-normal font-primary">Besides of being exposed to maintain our leadership program, there are several benefits as part of our scholarship.</div>
+  <div class="grid grid-cols-3 gap-x-5 px-48 py-10">
+    <div class="w-[78px] h-[74px] bg-gradient-to-b from-cyan-300 to-indigo-600 rounded-[10px]">
+      <img src="{asset5}" alt="TASLA Offline Leadership Program at UI" class="w-[78px] h-[74px]">
+      <div class="py-4 w-[291px] h-9 text-white text-2xl font-bold font-primary">Full tuition fee coverage</div>
+      <div class="break-all py-4 w-[354px] h-24 text-white text-xl font-normal font-primary">TASLA is dedicated to fully cover their scholars tuition fee. The tuition fee will be directly transferred to the corresponding scholar’s university.</div>
+    </div>
+    <div class="w-[78px] h-[74px] bg-gradient-to-b from-cyan-300 to-indigo-600 rounded-[10px]">
+      <img src="{asset6}" alt="TASLA Offline Leadership Program at UI" class="w-[78px] h-[74px]">
+      <div class="py-4 w-[291px] h-9 text-white text-2xl font-bold font-primary">Books Allowance </div>
+      <div class="break-all py-4 w-[354px] h-24 text-white text-xl font-normal font-primary">As part of the scholarship, TASLA scholars will obtain books allowance that will be granted for every semester of their study.</div>
+    </div>
+    <div class="w-[78px] h-[74px] bg-gradient-to-b from-cyan-300 to-indigo-600 rounded-[10px]">
+      <img src="{asset7}" alt="TASLA Offline Leadership Program at UI" class="w-[78px] h-[74px]">
+      <div class="py-4 w-[291px] h-9 text-white text-2xl font-bold font-primary">Networks</div>
+      <div class="break-all py-4 w-[393px] h-24 text-white text-xl font-normal font-primary">TASLA is has a network of several big tech companies, often we have the opportunities to invites CEOs to help mentor the scholars about leaderships.</div>
+    </div>
+  </div>
+</div>
+
+<div class="h-[150px] transition bg-gradient-to-b from-gray-700 to-black shadow"></div>
+
+<div class="section four bg-black px-10 py-5">
+  <div class=" py-1 text-white text-4xl font-black font-primary">How does the TASLA organization looks like?</div>
+  <div class=" py-3 text-white text-xl font-normal font-primary">Our scholarships' structure have 3 main roles inside of it.</div>
+  <div class=" py-1 text-white text-4xl font-black font-primary">The Board Members</div>
+  <div class=" py-3 text-white text-xl font-normal font-primary">The Board Members are those who supervised every TASLA proposed activity. About every month, TASLA scholars held a meeting to report their progresses and achievements for the month.</div>
+  <div class=" py-1 text-white text-4xl font-black font-primary">The Community Manager</div>
+  <div class=" py-3 text-white text-xl font-normal font-primary">The community manager is there to help and guide TASLA scholars for any TASLA related activity.</div>
+  <div class=" py-1 text-white text-4xl font-black font-primary">The scholars</div>
+  <div class=" py-3 text-white text-xl font-normal font-primary">Those who obtain the TASLA scholarships, every cohort has different amount of students selected as TASLA scholars. As for now, the application is only open for UI and ITB STEM students.</div>
+  <div class="circle">
+    <div class="py-5 text-white text-2xl font-normal font-primary">Learn more about the organization</div>
+  </div>
+</div>
