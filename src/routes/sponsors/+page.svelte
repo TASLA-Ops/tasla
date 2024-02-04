4e7f8c489e3ed1c949fb83c3b3a9075279c087f2
Lim Bodhi Wijaya <lim.bodhi@ui.ac.id>
2024-02-04 15:33:15 +0700

    about org and sponsors page done

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -5,6 +5,21 @@
 </script>
 
 <div class="bg-black min-h-screen flex flex-col px-5 py-5">
+	<h2 class="text-center text-white text-2xl md:text-3xl lg:text-4xl font-bold">Who are our sponsors?</h2>
+  <div class="flex flex-row justify-center items-center gap-4 md:gap-6 p-4 md:p-8">
+    <div class="flex justify-center items-center  rounded-lg shadow-lg w-1/6 p-4">
+      <img class="h-full w-full object-contain" src="{glints}" alt="Glints" />
+    </div>
+    <div class="flex justify-center items-center  rounded-lg shadow-lg w-1/6 p-4">
+      <img class="h-full w-full object-contain" src="{JungleVentures}" alt="Jungle Ventures" />
+    </div>
+    <div class="flex justify-center items-center  rounded-lg shadow-lg w-1/3 p-4">
+      <img class="h-full w-full object-contain" src="{mhv}" alt="Monk's Hill Ventures" />
+    </div>
+  </div>
+  <div class="text-center">
+
+  </div>
 	<div class="text-white text-4xl font-black font-primary py-5">Interested to be our sponsor?</div>
 	<div class="text-justify text-white text-2xl font-normal font-primary py-5">By being TASLA sponsor, you are helping Indonesia to nurture more future leaders. A lot of our Alumni has proven a significant contribution throughout the Indonesia ecosystem. This can be part of your contribution too!</div>
 	<div class="text-white text-4xl font-black font-primary py-5">Contact us!</div>
